0beb9e29d57a71e9fdca503a41c18450a99ffb0a
nnguyen282 <nhatthanh.nguyen@dxc.com>
2023-06-05 10:04:09 +0700

    First commit

---
+++ src/test/java/robocon321/com/hello_world/AppTest.java
@@ -0,0 +1,20 @@
+package robocon321.com.hello_world;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
